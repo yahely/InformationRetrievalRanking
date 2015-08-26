73bae97851c1145c35ce49cb126c61783ddb5845
yahely <yahel.yechieli@gmail.com>
2015-08-26 20:34:54 +0300

    adding external lucene jars

---
+++ src/Indexer.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class Indexer {
+
+
+}
